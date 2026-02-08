3c09ca3e6aba99278002fd3650ecc2e9fc05f415
Liana-K <lianakarim77@gmail.com>
2026-02-08 08:35:04 +0000

    "deleted extra files"

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,18 +1,27 @@
 
-import Splash from "./pages/Splash";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Record from "./pages/Record";
-import History from "./pages/History";
+// import Splash from "./pages/Splash";
+// import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import Record from "./pages/Record";
+// import History from "./pages/History";
 
-export default function App() {
-  const [page, setPage] = useState("splash");
 
-  if (page === "splash") return <Splash goTo={setPage} />;
-  if (page === "login") return <Login goTo={setPage} />;
-  if (page === "dashboard") return <Dashboard goTo={setPage} />;
-  if (page === "record") return <Record goTo={setPage} />;
-  if (page === "history") return <History goTo={setPage} />;
+// const root = document.getElementById("root");
 
-  return null;
-}
+// createRoot(root).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// export default function App() {
+//   return <h1>Hello — React is working</h1>;
+//   // const [page, setPage] = useState("splash");
+
+//   // if (page === "splash") return <Splash goTo="./pages/Splash.jsx" />;
+//   // if (page === "login") return <Login goTo="./pages/Login.jsx" />;
+//   // if (page === "dashboard") return <Dashboard goTo= "./pages/Dashboard.jsx" />;
+//   // if (page === "record") return <Record goTo="./pages/Record.jsx" />;
+//   // if (page === "history") return <History goTo="./pages/History.jsx" />;
+
+//   return null;
+// }
--- frontend/my-react-app/src/main.jsx
+++ frontend/my-react-app/src/main.jsx
@@ -1,12 +1,20 @@
-import App from "./App";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-import "./styles/charts.css";
-import "./styles/layout.css";
-import "./styles/buttons.css";
-import "./styles/dashboard.css";
+// const root = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// createRoot(root).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // import "./styles/charts.css";
+// // import "./styles/layout.css";
+// // import "./styles/buttons.css";
+// // import "./styles/dashboard.css";
+
+// // ReactDOM.createRoot(document.getElementById("root")).render(
+// //     <App />
+// // );
