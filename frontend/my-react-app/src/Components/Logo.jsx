381610eef5501b69e1df25ddf13879f87a93fdec
Khwaish_Kapoor <130338279+KKapoor0001@users.noreply.github.com>
2026-02-08 00:00:06 +0000

    react changes

---
+++ frontend/my-react-app/src/Components/Logo.jsx
@@ -0,0 +1,14 @@
+import logo from "../assets/jointscope-logo.png";
+
+export default function Logo({ size = 180 }) {
+  return (
+    <img
+      src={logo}
+      alt="JointScope Logo"
+      style={{
+        width: size,
+        height: "auto"
+      }}
+    />
+  );
+}
